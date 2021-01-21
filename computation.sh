75d0256f9a2f20854895e66bc6bd47b6191eb16d
Pranil Kakade <77184928+pranilkakade111@users.noreply.github.com>
2021-01-21 17:00:29 +0530

    Update computation.sh

--- computation.sh
+++ computation.sh
@@ -2,10 +2,10 @@ read -p "The first number=" a
 read -p "the second number=" b
 read -p "the third number=" c
 
-compute1=$a+$b*$c
-compute2=$a*$b+$c
-compute3=$c+$a/$b
-compute4=$a%$b+$c
+compute1=$(($a+$b*$c))
+compute2=$(($a*$b+$c))
+compute3=$(($c+$a/$b))
+compute4=$(($a%$b+$c))
 echo $compute1
 echo $compute2
 echo $compute3
