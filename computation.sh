8e4e35fffa2ef48502e92db506ba9ad3eff6b06d
Pranil Kakade <pranilkakade111@gmail.com>
2021-01-21 17:55:07 +0530

    Adding:Store the result in dictinory

--- computation.sh
+++ computation.sh
@@ -1,12 +1,20 @@
 read -p "The first number=" a
 read -p "the second number=" b
 read -p "the third number=" c
+declare -A results
+compute1=$(($a+$b*$c))
+compute2=$(($a*$b+$c))
+compute3=$(($c+$a/$b))
+compute4=$(($a%$b+$c))
 
-compute1=$a+$b*$c
-compute2=$a*$b+$c
-compute3=$c+$a/$b
-compute4=$a%$b+$c
 echo $compute1
 echo $compute2
 echo $compute3
 echo $compute4
+
+results[compute1]="$compute1"
+results[compute2]="$compute2"
+results[compute3]="$compute3"
+results[compute4]="$compute4"
+echo "All results=" ${results[@]}
+echo ${!results[@]}
